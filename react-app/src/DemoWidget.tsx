c0ecbdc606948986f0d514444db119f5f6e7bee6
Tobias Jacob <tobias.jacob@kit.edu>
2022-11-14 11:36:50 +0100

    Send text box

--- react-app/src/DemoWidget.tsx
+++ react-app/src/DemoWidget.tsx
@@ -1,17 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
 import { CommunicatorContext, usePrinterCallback } from './Communicator';
 import { Demo } from './ice/Printer';
 
 
 function DemoWidget() {
+  const [msg, setMsg] = useState("");
+
   const sendStuff = usePrinterCallback(async (printer) => {
-    await printer.printString("Hello world");
+    await printer.printString("Client to cpp: " + msg);
   })
 
   return (
     <div className="App">
       <header className="App-header">
+        <textarea value={msg} onChange={(ev) => {setMsg(ev.target.value)}} />
         <button onClick={sendStuff}>Send command</button>
       </header>
     </div>
